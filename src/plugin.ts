6040a75529e03c5a8f971b444252c7edfc50710e
Kathleen Auerbach <k.auerbach@stroeermediabrands.de>
2021-05-12 10:13:48 +0200

    VP-60 add play by click and revolverplay

--- src/plugin.ts
+++ src/plugin.ts
@@ -3,47 +3,85 @@ import noop from './noop'
 import logger from './logger'
 import { fetchAPI, transformData } from './api'
 import getCard from './template'
-import { IStroeerVideoplayer, IEndcardOptions } from '../types/types'
+import { IData, IStroeerVideoplayer, IEndcardOptions } from '../types/types'
 
 class EndcardPlugin {
   videoplayer: IStroeerVideoplayer
   videoElement: HTMLVideoElement
-  videoElementWidth: number
-  videoElementHeight: number
   endcardContainer: HTMLDivElement
-  endcardUri: string | null
-  OnLoadedCallback: Function
-  OnClickCallback: Function
-  OnRevolverplayCallback: Function
-  OnRevolverplayPauseCallback: Function
+  endcardUrl: string | null
+  onLoadedCallback: Function
+  onClickCallback: Function
+  onRevolverplayCallback: Function
+  onRevolverplayPauseCallback: Function
   dataKeyMap: object
+  transformedData: IData[]
   showEndcard: boolean
+  uiEl: HTMLDivElement
+  revolverplayTime: number
+  intervalTicker: NodeJS.Timeout | null
 
   constructor (stroeervideoplayer: IStroeerVideoplayer, opts: IEndcardOptions = {}) {
     this.videoplayer = stroeervideoplayer
-    // https://github.com/stroeer/stroeer-videoplayer/blob/main/src/StroeerVideoplayer.ts
     this.videoElement = stroeervideoplayer.getVideoEl()
-    this.videoElementWidth = this.videoElement.clientWidth
-    this.videoElementHeight = this.videoElement.clientHeight
 
-    // TODO: maybe add get and set for endcard-url in videoplayer
-    this.endcardUri = this.videoElement.getAttribute('data-endcard-url')
+    this.endcardUrl = this.videoElement.getAttribute('data-endcard-url')
     this.dataKeyMap = opts.dataKeyMap !== undefined ? opts.dataKeyMap : noop
+    this.transformedData = []
     this.showEndcard = true
+    this.revolverplayTime = opts.revolverplayTime !== undefined ? opts.revolverplayTime : 5
+    this.intervalTicker = null
 
-    this.OnLoadedCallback = opts.OnLoadedCallback !== undefined ? opts.OnLoadedCallback : noop
-    this.OnClickCallback = opts.OnClickCallback !== undefined ? opts.OnClickCallback : noop
-    this.OnRevolverplayCallback = opts.OnRevolverplayCallback !== undefined ? opts.OnRevolverplayCallback : noop
-    this.OnRevolverplayPauseCallback = opts.OnRevolverplayPauseCallback !== undefined ? opts.OnRevolverplayPauseCallback : noop
+    this.onLoadedCallback = opts.onLoadedCallback !== undefined ? opts.onLoadedCallback : noop
+    this.onClickCallback = opts.onClickCallback !== undefined ? opts.onClickCallback : noop
+    this.onRevolverplayCallback = opts.onRevolverplayCallback !== undefined ? opts.onRevolverplayCallback : noop
+    this.onRevolverplayPauseCallback = opts.onRevolverplayPauseCallback !== undefined ? opts.onRevolverplayPauseCallback : noop
+
+    this.uiEl = stroeervideoplayer.getUIEl()
 
     this.endcardContainer = document.createElement('div')
     this.endcardContainer.classList.add('plugin-endcard-container', 'hidden')
-
-    // this.videoElement.after(this.endcardContainer)
-    const uiEl = stroeervideoplayer.getUIEl()
-    uiEl.appendChild(this.endcardContainer)
+    this.videoElement.after(this.endcardContainer)
 
     return this
+  }
+
+  revolverplay = (): void => {
+    const progressSvgCircle: HTMLElement | null = this.endcardContainer.querySelector('.plugin-endcard-progress-value')
+    const radius = 54
+    const circumference = 2 * Math.PI * radius
+    let remainingTime = this.revolverplayTime
+
+    const progress = (value: number): void => {
+      const p = value / 100
+      const dashoffset = circumference * (1 - p)
+      if (progressSvgCircle !== null) {
+        progressSvgCircle.style.strokeDashoffset = String(dashoffset)
+      }
+    }
+
+    const ticker = (): void => {
+      progress(((this.revolverplayTime - remainingTime) / this.revolverplayTime) * 100)
+      remainingTime--
+      if (remainingTime < -1) {
+        this.play(0)
+      }
+    }
+
+    ticker()
+    this.intervalTicker = setInterval(ticker, 1000)
+  }
+
+  play = (idx: number): void => {
+    if (this.intervalTicker !== null) {
+      clearInterval(this.intervalTicker)
+    }
+    const videoSources = this.transformedData[idx].sources
+    this.endcardUrl = this.transformedData[idx].endcard_url
+    this.videoplayer.setSrc(videoSources)
+    this.videoplayer.load()
+    this.videoplayer.play()
+    this.hide()
   }
 
   addClickEvents = (): void => {
@@ -53,28 +91,37 @@ class EndcardPlugin {
     cards.forEach(card => {
       card.addEventListener('click', (e) => {
         e.preventDefault()
-        this.OnClickCallback(this.videoElement)
+        this.onClickCallback(this.videoElement)
+
+        const el = (e.target as Element).closest('[data-role="plugin-endcard-card"]')
+        const idx: string | null = el !== null ? el.getAttribute('data-idx') : null
+        if (idx === null) return
+        this.play(parseInt(idx))
       })
     })
 
     if (pauseButton === null) return
     pauseButton.addEventListener('click', (e) => {
       e.preventDefault()
-      this.OnRevolverplayPauseCallback(this.videoElement)
+      e.stopPropagation()
+      this.onRevolverplayPauseCallback(this.videoElement)
+      if (this.intervalTicker !== null) {
+        clearInterval(this.intervalTicker)
+      }
     })
   }
 
   render = (): void => {
-    if (this.endcardUri === null) return
+    if (this.endcardUrl === null) return
 
-    fetchAPI<object>(
-      this.endcardUri)
+    fetchAPI<object>(this.endcardUrl)
       .then((data) => {
-        const transformedData = transformData(data, this.dataKeyMap)
-        logger.log(transformedData)
+        this.transformedData = transformData(data, this.dataKeyMap)
+        logger.log(this.transformedData)
 
+        this.endcardContainer.innerHTML = ''
         for (let i: number = 0; i < 6; i++) {
-          const cardTemplate = getCard(i, transformedData[i])
+          const cardTemplate = getCard(i, this.transformedData[i])
           this.endcardContainer.innerHTML += cardTemplate
         }
       })
@@ -84,11 +131,16 @@ class EndcardPlugin {
       })
   }
 
+  hide = (): void => {
+    this.uiEl.classList.remove('hidden')
+    this.endcardContainer.classList.add('hidden')
+  }
+
   show = (): void => {
     if (!this.showEndcard) return
-    this.videoplayer.deinitUI(this.videoplayer.getUIName())
+    this.uiEl.classList.add('hidden')
     this.endcardContainer.classList.remove('hidden')
-    this.OnLoadedCallback(this.videoElement)
+    this.onLoadedCallback(this.videoElement)
   }
 }
 
@@ -103,12 +155,13 @@ const plugin = {
     // for development change "contentVideoFirstQuartile" to "loadedmetadata"
     videoEl.addEventListener('contentVideoFirstQuartile', () => {
       endcardPlugin.render()
-      endcardPlugin.addClickEvents()
     })
 
     // for development change "contentVideoEnded" to "loadedmetadata"
     videoEl.addEventListener('contentVideoEnded', () => {
+      endcardPlugin.addClickEvents()
       endcardPlugin.show()
+      endcardPlugin.revolverplay()
     })
   },
   deinit: () => {
--- src/template.ts
+++ src/template.ts
@@ -2,13 +2,19 @@ import { IData } from '../types/types'
 
 const getCard = (index: number, obj: IData): string => {
   const template = `
-    <div class="plugin-endcard-card" data-role="plugin-endcard-card">
+    <div class="plugin-endcard-card" data-idx="${index}" data-role="plugin-endcard-card">
       <div class="plugin-endcard-thumbnail" style="background-image: url(${obj.image});"></div>
       <span class="plugin-endcard-title">${obj.title}</span>
       ${index === 0
 ? `
-        <button>Play SVG with animation</button>
-        <button data-role="plugin-endcard-pause">Anhalten</button>
+        <div class="plugin-endcard-play-icon">
+          <svg class="plugin-endcard-progress-icon" viewBox="0 0 120 120">
+            <circle class="plugin-endcard-progress-meter"  cx="60" cy="60" r="54" fill="none" stroke="#e6e6e6" stroke-width="3" />
+            <circle class="plugin-endcard-progress-value" cx="60" cy="60" r="54" fill="none" stroke="#f77a52" stroke-width="3" stroke-dasharray="339.292" stroke-dashoffset="339.292" />
+            <path fill="#e6e6e6" d="m 60.940678,83.618644 -25,-32 h 51 z"/>
+          </svg>
+        </div>
+        <button class="plugin-endcard-pause" data-role="plugin-endcard-pause">Anhalten</button>
       `
 : ''}
     </div>

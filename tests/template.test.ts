d5f94715797df657e598139efc9d72a98b5c5205
Kathleen Auerbach <k.auerbach@stroeermediabrands.de>
2021-05-28 11:06:32 +0200

    VP-88 add test for template.ts

--- tests/template.test.ts
+++ tests/template.test.ts
@@ -1,7 +1,47 @@
-import { getTile } from '../src/template'
+import { getTile, getTileReplay } from '../src/template'
 
-it('should be a function', () => {
-  expect(typeof getTile).toBe('function')
+const dataObj = [
+  {
+    title: 'Title of tile number one',
+    image: 'https://videos.giga.de/files/1307753225/480p.jpg',
+    sources: [],
+    endpoint: '',
+  },
+  {
+    title: 'Title of tile number two',
+    image: 'https://videos.giga.de/files/1307753225/720p.jpg',
+    sources: [],
+    endpoint: '',
+  },
+  {
+    title: 'Title of tile number three',
+    image: 'https://videos.giga.de/files/1307753225/1080p.jpg',
+    sources: [],
+    endpoint: '',
+  }
+]
+
+test('Tile markup is rendered as expected when revolverplay is active', () => {
+  document.body.innerHTML = '<div id="testContainer"></div>'
+  const stage = document.querySelector('#testContainer')!
+  for (let i = 0; i < dataObj.length; i++) {
+    stage.innerHTML += getTile(i, dataObj[i], 2)
+  }
+  expect(stage).toMatchSnapshot()
 })
 
-// add more when html for cards are final
+test('Tile markup is rendered as expected when revolverplay is not active', () => {
+  document.body.innerHTML = '<div id="testContainer"></div>'
+  const stage = document.querySelector('#testContainer')!
+  for (let i = 0; i < dataObj.length; i++) {
+    stage.innerHTML += getTile(i, dataObj[i], 0)
+  }
+  expect(stage).toMatchSnapshot()
+})
+
+test('Replay tile markup is rendered as expected', () => {
+  document.body.innerHTML = '<div id="testContainer"></div>'
+  const stage = document.querySelector('#testContainer')!
+  stage.innerHTML = getTileReplay('https://videos.giga.de/files/1307753225/720p.jpg')
+  expect(stage).toMatchSnapshot()
+})
